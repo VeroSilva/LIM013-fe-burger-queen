7ee48678e31a1be73d5725c2dfe276b89a0a3b56
VeroFlores <veritocalla@gmail.com>
2021-01-02 09:39:46 -0500

    add getData

--- src/components/kitchen.js
+++ src/components/kitchen.js
@@ -1,20 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import { KitchenOrder } from './kitchenOrder';
+import { db } from '../firebase';
 
 export const Kitchen = (props) => {
-
+const [showOrder, setShowOrder] = useState([]);
   // let items =  props.orderList[index].items;
-
+  
+  useEffect(()=>{
+    db.collection('orders').onSnapshot((doc) => {
+      const arrayMenu =[]
+      doc.forEach((el)=>{
+        arrayMenu.push({
+          id:el.id,
+          ...el.data()
+        });
+      })
+      setShowOrder(arrayMenu)
+    })
+    
+  }, []);
+console.log(showOrder);
   return (
     <div>
-      {props.listOrder.map((order, index) => <div key={index}>
+      {showOrder.map((order,index)=>
+        <ul key={order.id}>
+          <p>Pedido Nro.{index+1}</p>
+          <p>Status:{order.status}</p>
+          <p>Tiempo</p>
+          <p>Detalle de Pedido</p>
+          {order.order.map((element,index)=>
+          <p key={'o'+index}>{element.description}</p>)}
+        </ul>
+
+      )}
+      {/* {props.listOrder.map((order, index) => <div key={index}>
         <h1>Pedido {index + 1 }: </h1>
         <ul>
           {order.item.map((element, index)=> <li key={index + Math.random()}>{element.description}</li>)}
         </ul>
         <label>Estado</label>
       </div>
-      )}
+      )} */}
     </div>
   )
 };
