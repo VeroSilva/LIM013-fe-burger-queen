0e5a3e923c9ff6855589a3371a9a7a7fc1b9b52e
VeroFlores <veritocalla@gmail.com>
2021-01-02 09:44:18 -0500

    create collection

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { db } from '../firebase';
 
 import { Kitchen } from './kitchen'
 import { OrderView } from './orderView';
@@ -7,14 +8,34 @@ export const Restaurant = () => {
   
   const [listOrder, setListOrder] = useState([]);
 
-  const addOrder = (order) => {
-    setListOrder([...listOrder, {item: order, date: new Date()}]);
+  const addOrder = (orders) => {
+    // setListOrder([...listOrder, {item: orders, date: new Date()}]);
+    // console.log(orders);
+    // orders.map((order,index) => {
+      db.collection('orders').doc().set({
+        time:new Date(),
+        order:orders,
+        status:'Pending',
+        
+        
+      });
+
+    // setListOrder([...listOrder, {item: order, date: new Date()}]);
+    
+    
   };
+  console.log(listOrder);
 
   return (
     <div>
       <OrderView addOrder={addOrder}/>
-      <Kitchen listOrder={listOrder}/>
+      <Kitchen/>
     </div>
   )
 };
+
+// {props.listOrder.map((order, index) => <div key={index}>
+// <h1>Pedido {index + 1 }: </h1>
+// <ul>
+//   {order.item.map((element, index)=> <li key={index + Math.random()}>{element.description}</li>)}
+// </ul>
