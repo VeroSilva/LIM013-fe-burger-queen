5d94e26b1388684c18ba8a8c012ffb6378259c96
VeroFlores <veritocalla@gmail.com>
2021-01-13 18:11:40 -0500

    add activeClassName  Nabvar using react router dom

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import {Link} from "react-router-dom";
-
+import {NavLink} from "react-router-dom";
+import '../styles/App.css'
 export const Navbar = (props) => {
 
   console.log(props);
     return (
       <header>
         <img src="https://user-images.githubusercontent.com/68167686/103605203-4e1c0780-4ee1-11eb-8c96-0d1379f88bf5.png" alt=""/>
-        <div className="directions-links">
-          <Link to={{
+        <nav className="directions-links">
+          <ul>
+            <li>
+            <NavLink to={{
             pathname:'/waiter',
             state: props.nroNotifications,
-          }}>Waiter</Link>
-          <Link to="/kitchen">Kitchen</Link>
-        </div>
+          }}
+          activeClassName="active">Waiter</NavLink>
+            </li>
+            <li>
+              <NavLink to="/kitchen" activeClassName="active">Kitchen</NavLink>
+            </li>
+
+
+          </ul>
+
+        </nav>
       </header>
     )
 }
